f5115ed68947468962ec1fd027c77e52d8236951
FredrikEglese <fredrik.eglese@gmail.com>
2020-06-07 15:56:20 +0100

    Modifying state of square now in the board comp

--- tic-tac-two/src/index.js
+++ tic-tac-two/src/index.js
@@ -3,28 +3,40 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            value: null,
-        };
-    }
-
-    render() {
-      return (
-        <button 
-            className="square"
-            onClick={()=>this.setState({value: "X"})}
-        >
-          {this.state.value}
-        </button>
-      );
-    }
+  render() {
+    return (
+      <button 
+        className="square"
+          onClick={()=>this.props.onClick()}
+      >
+        {this.props.value}
+      </button>
+    );
   }
+}
   
   class Board extends React.Component {
+
+    constructor(props){
+      super(props);
+      this.state = {
+        squares: Array(9).fill(null),
+      }
+    }
+    
+    handleClick(i) {
+      const squares = this.state.squares.slice(); // Take a coppy of current state
+      squares[i] = 'X'; // Modify one of the values
+      this.setState({squares: squares}); // Assign the modified state
+    }
+
     renderSquare(i) {
-      return <Square value={i}/>;
+      return (
+        <Square
+          value={this.state.squares[i]}
+          onClick={() => this.handleClick(i)}
+        />
+      );
     }
   
     render() {
