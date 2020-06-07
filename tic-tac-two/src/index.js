831cebb442bf4cc688e6f0b41718cde845925dee
FredrikEglese <fredrik.eglese@gmail.com>
2020-06-07 16:49:35 +0100

    Add winning state

--- tic-tac-two/src/index.js
+++ tic-tac-two/src/index.js
@@ -21,13 +21,21 @@ function Square(props){
       super(props);
       this.state = {
         squares: Array(9).fill(null),
+        xIsNext: true,
       }
     }
     
     handleClick(i) {
       const squares = this.state.squares.slice(); // Take a coppy of current state
-      squares[i] = 'X'; // Modify one of the values
-      this.setState({squares: squares}); // Assign the modified state
+      if (calculateWinner(squares) || squares[i]){
+        return;
+      }
+     
+      squares[i] = this.state.xIsNext ? 'X' : 'O'; // Modify one of the values
+      this.setState({
+          squares: squares,
+          xIsNext: !this.state.xIsNext,
+        });
     }
 
     renderSquare(i) {
@@ -40,8 +48,14 @@ function Square(props){
     }
   
     render() {
-      const status = 'Next player: X';
-  
+      const winner = calculateWinner(this.state.squares);
+      let status;
+      if (winner) {
+        status = 'Winner ' + winner;
+      } else {
+        status = 'Next player: ' + (this.state.xIsNext? 'X' : 'O');
+      }
+      
       return (
         <div>
           <div className="status">{status}</div>
@@ -79,6 +93,26 @@ function Square(props){
         </div>
       );
     }
+  }
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
   }
   
   // ========================================
