8d72ee3d75ce38686c5d0289ee64597366ce06c3
FredrikEglese <fredrik.eglese@gmail.com>
2020-06-08 23:02:19 +0100

    Added display/delete functionality
    First commit with the basics needed for displaying and deleting items from a to do list

--- todolist/src/index.js
+++ todolist/src/index.js
@@ -1,9 +1,60 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+// Square is now a function component because it's only contains
+// a render method, so it doesn't need as much object detail
+  
+  class ListItem extends React.Component {    
+    render() {
+      return (
+        <li key={this.props.key}>{this.props.text} <a onClick={this.props.onClick}>X</a></li>
+      );
+    }
+  }
+  
+  class List extends React.Component {
+
+    constructor(props){
+      super(props);
+      this.state = {
+        itemList: [
+          {key:1,text:"Dishes"},
+          {key:2,text:"Take out the bins"},
+          {key:3,text:"Make dinner"},
+        ],
+      }
+    }
+
+    deleteItem(key){
+      const tmpItemList = this.state.itemList.slice().filter(obj => {return obj.key !== key;})
+      this.setState({
+        itemList: tmpItemList,
+      });
+    }
+
+    render() {
+      const itemsList = this.state.itemList.map((item)=>
+        <ListItem 
+          key={item.key}
+          text={item.text}
+          onClick={()=>this.deleteItem(item.key)}
+        />
+      );
+
+      return (
+        <ul>
+          {itemsList}
+        </ul>
+      );
+    }
+  }
+
+  
+  // ========================================
+  
+  ReactDOM.render(
+    <List />,
+    document.getElementById('root')
+  );
+  
