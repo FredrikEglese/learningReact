edffe1e5485b44fd4dcbb731b394fc48a07efc8c
FredrikEglese <fredrik.eglese@gmail.com>
2020-06-15 22:36:59 +0100

    Roughly following w3 tutorial and some other bits

--- scratch/src/App.js
+++ scratch/src/App.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Car from './Car.js';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Helo World!</h1>
-      </div>
+      <Garage />
     );
+  }
+}
+
+class Garage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome to my garage!</h1>
+        <Car model="Monster Truck"/>
+      </div>
+    )
   }
 }
 
