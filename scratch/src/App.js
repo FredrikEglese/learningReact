57ca97e02f36b5685bd8ad326639c089a256e7d3
FredrikEglese <fredrik.eglese@gmail.com>
2021-01-05 18:20:59 +0000

    Cleanup

--- scratch/src/App.js
+++ scratch/src/App.js
@@ -1,23 +1,10 @@
 import React, { Component } from 'react';
-import './App.css';
-import Car from './Car.js';
 
 class App extends Component {
   render() {
     return (
-      <Garage />
+      <h1>Hello world!</h1>
     );
-  }
-}
-
-class Garage extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Welcome to my garage!</h1>
-        <Car model="Monster Truck"/>
-      </div>
-    )
   }
 }
 
